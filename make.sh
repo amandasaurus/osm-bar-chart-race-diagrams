245ef57280450b6e39b86a4161284b9eed078cb8
Amanda McCann <amanda@technomancy.org>
2022-08-31 21:40:39 +0200

    misc

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@ set -o errexit -o nounset
 FILENAME="$(realpath "${1:?arg 1 must be input filename}")"
 
 rm -f output.csv
-osm-tag-csv-history -i "$FILENAME"  -o ./raw_csv_tag_changes.csv -t building -C old_value,new_value,iso_timestamp
+osm-tag-csv-history -v -i "$FILENAME"  -o ./raw_csv_tag_changes.csv -t building -C old_value,new_value,iso_timestamp
 
 psql -c "CREATE TABLE IF NOT EXISTS raw_tag_changes (old_value TEXT, new_value TEXT, iso_timestamp TIMESTAMP);"
 psql -c "CREATE INDEX IF NOT EXISTS raw_tag_changes_timestamp ON raw_tag_changes (iso_timestamp);"
@@ -19,8 +19,8 @@ psql -c "CREATE MATERIALIZED VIEW dates AS ( select to_char( generate_series( (s
 psql -c "DROP MATERIALIZED VIEW IF EXISTS t_to_pivot;"
 psql -c "CREATE MATERIALIZED VIEW t_to_pivot AS ( select values.new_value as tag_value, dates.to_char, sum(case when raw_tag_changes.old_value = values.new_value and raw_tag_changes.new_value = values.new_value then 0 when raw_tag_changes.old_value = values.new_value then -1 when raw_tag_changes.new_value = values.new_value then +1 else 0 end) as value_total from values, dates, raw_tag_changes where raw_tag_changes.iso_timestamp <= dates.to_char::timestamp and (raw_tag_changes.new_value = values.new_value OR raw_tag_changes.old_value = values.new_value) group by (values.new_value, dates.to_char) order by 1, 2);"
 
-psql -At -c "COPY (select * from t_to_pivot order by 1,2) TO STDOUT " | sort > tab
-datamash -s crosstab 1,2 sum 3 < tab > crosstab_for_flouris.tsv
+psql -At -c "COPY (select * from t_to_pivot order by 1,2) TO STDOUT " | sort > table_to_pivot.tsv
+datamash -s crosstab 1,2 sum 3 < table_to_pivot.tsv > crosstab_for_flourish.tsv
 #
 #rm -f tab
 #psql -At -c "select distinct new_value from raw_tag_changes where new_value <> '' order by new_value;" | sponge | while read VALUE ; do
